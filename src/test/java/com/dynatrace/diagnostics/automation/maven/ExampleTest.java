65460eb9402b5216a1bbf7fa826895536a407ccb
Łukasz Hamerszmidt <lukasz.hamerszmidt@dynatrace.com>
2016-08-18 13:26:24 +0200

    Modified build script to allow testing functionality

---
+++ src/test/java/com/dynatrace/diagnostics/automation/maven/ExampleTest.java
@@ -0,0 +1,9 @@
+package com.dynatrace.diagnostics.automation.maven;
+
+import org.apache.maven.plugin.testing.AbstractMojoTestCase;
+
+public class ExampleTest extends AbstractMojoTestCase {
+    public void test() {
+        assertTrue(true);
+    }
+}
